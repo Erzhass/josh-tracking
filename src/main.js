6651f824efd55d2eb2e667977dd5279fa111cf96
Erzhass <fardinyondaktau@gmail.com>
2026-07-17 18:46:18 +0700

    js

---
+++ src/main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    minWidth: 1024,
+    minHeight: 768,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: false,
+      contextIsolation: true
+    },
+    // Menghilangkan menu bar bawaan agar UI terlihat clean seperti di mockup
+    autoHideMenuBar: true 
+  });
+
+  // Sementara me-load file index.html yang akan kita buat di folder src
+  win.loadFile(path.join(__dirname, 'index.html'));
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
